28bf480423805779895118e94a8794ed44ac56c6
Killian-LeClainche <kleclain@purdue.edu>
2017-03-25 16:42:25 -0400

    Working on left side integration.

--- website/js/template.js
+++ website/js/template.js
@@ -9,8 +9,16 @@ $("document").ready(function()
     url = "/Test";
     console.log("rooms" + url);
 
-    var room = database.ref("rooms" + url + "/songs");
-    room.on("value", function(snapshot)
+    var room = database.ref("rooms" + url);
+    var songs = database.ref("rooms" + url + "/songs");
+
+    room.once("value", function(snapshot)
+    {
+        $("#room-name").text(snapshot.val().name);
+        $("#room-code").text("#" + url.substring(1));
+    });
+
+    songs.on("value", function(snapshot)
     {
         console.log("UPDATED!");
         var songsObject = snapshot.val();
@@ -34,7 +42,11 @@ $("document").ready(function()
 
         for (var i in songsObject)
         {
-            updateSong(rightContainer, songsObject[i]);
+            var song = songsObject[i];
+            var songElement = children[song.position];
+            songElement.id = i;
+
+            console.log(songElement);
         }
     });
 });

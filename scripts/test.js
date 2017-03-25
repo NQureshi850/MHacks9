9a51daf3e291c361c7a8da378539ae0a342c4f8f
Owloid <jarett.lee.pi@gmail.com>
2017-03-25 11:03:42 -0400

    Add test database

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,14 @@
 /* global config:false, firebase:false */
 
+assert(config != undefined);
+assert(firebase != undefined);
+
+// initialize firebase
+firebase.initializeApp(config);
+const database = firebase.database();
+
 function assert(bool, message) {
     if (!bool)  {
         throw new Error(message || "");
     }
 }
-
-assert(config != undefined);
-firebase.initializeApp(config);
